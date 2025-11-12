956a9bcbf493070f82ff1baab8e3dd84c29a8bc9
unknown <michaelrafanantenana@gmail.com>
2025-11-12 14:14:03 +0100

    Ajout du contenu initial de l'application

---
+++ page/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Platform from '../components/Platform'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Coopération Sociale - Plateforme d'entraide</title>
+        <meta name="description" content="Générez des revenus passifs ensemble sur les réseaux sociaux" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Platform />
+    </>
+  )
+}
